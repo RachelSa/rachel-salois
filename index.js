142b3d0560d689a9a3b96ac0cbac93ae40b51bfa
github username <rachel.salois@gmail.com>
2017-03-15 20:47:30 -0400

    test loading problem

--- index.js
+++ index.js
@@ -1,6 +1,69 @@
-$.getScript( "content.js", function() {
-  console.log( "What is the difference between a cat and a comma?" )
-})
+// $.getScript( "content.js", function() {
+//   console.log( "What is the difference between a cat and a comma?" )
+// })
+
+const CONTENT = {
+  websites: {
+    content: [
+      "I like to make cool websites",
+      "." 
+      ]
+    },
+  blog: { 
+    content: [
+      "On my ", 
+      "<a title=\"blog\" href=\"https://rachelscodenotes.wordpress.com/\" target=\"_blank\">blog</a>", 
+      ", I write about some of my more interesting programming-related discoveries",
+      "." 
+      ]
+    }, 
+  twitter: { 
+    content: [
+      "Sometimes I ",
+      "<a title=\"twitter\" href=\"https://twitter.com/rachel_salois\" target=\"_blank\">tweet</a>",
+      "."
+      ]
+    }, 
+  rails: { 
+    content: [
+      "I've made some fun projects using frameworks and libraries such as ",
+      "<a title=\"rails\" href=\"https://github.com/bobertoo/pretty-kitty-city\" target=\"_blank\">Rails</a>",
+      ", ",
+      "<a title=\"jQuery\" href=\"https://github.com/clintonn/dont-read-the-comments\" target=\"_blank\">jQuery</a>",
+      ", ",
+      "<a title=\"React-Redux\" href=\"https://github.com/acohn93/react-redux-playlist-buddy\" target=\"_blank\">React, and Redux</a>",
+      "." 
+      ]
+    },  
+  career: { 
+    content: [
+      "Fun fact about my ",
+      "<a title=\"career\" href=\"https://www.linkedin.com/in/rachel-salois-a07a2320/\" target=\"_blank\">career</a>",
+      ":",
+      " I have read (for work) more post-apocalyptic novels than anyone I know",
+      "."
+      ]
+    },
+  cactus: { 
+    content: [
+      "Occasionally",
+      ",",
+      " in my spare time",
+      ",",
+      " I care for a cactus that lives at my apartment",
+      "."
+    ]
+  },
+  look: { 
+    content: [
+      "This is pretty much what it looks like",
+      ",",
+      " give or take a few prickles",
+      "."
+   ]
+  }  
+}
+
 
 $( document ).ready(function(){
   console.log( "One has the paws before the claws and the other has the clause before the pause." )
