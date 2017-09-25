55bd95fa20cd1abd8d015091b38753e629b151ff
Rachel Salois <rachel.salois@gmail.com>
2017-09-25 18:32:29 -0400

    simplified and changed to vanilla js

--- content.js
+++ content.js
@@ -1,61 +1,28 @@
-const CONTENT = {
-  websites: {
-    content: [
-      "I like to make cool websites",
-      "."
-      ]
-    },
-  blog: {
-    content: [
-      "On my ",
-      "<a title=\"blog\" href=\"https://rachelscodenotes.wordpress.com/\" target=\"_blank\">blog</a>",
-      ", I write about some of my more interesting programming-related discoveries",
-      "."
-      ]
-    },
-  twitter: {
-    content: [
-      "Sometimes I ",
-      "<a title=\"twitter\" href=\"https://twitter.com/rachel_salois\" target=\"_blank\">tweet</a>",
-      "."
-      ]
-    },
-  rails: {
-    content: [
-      "I've made some fun projects using ",
-      "<a title=\"rails\" href=\"https://github.com/bobertoo/pretty-kitty-city\" target=\"_blank\">Rails</a>",
-      ", ",
-      "<a title=\"jQuery\" href=\"https://github.com/clintonn/dont-read-the-comments\" target=\"_blank\">jQuery</a>",
-      ", ",
-      "<a title=\"React-Redux\" href=\"https://github.com/acohn93/react-redux-playlist-buddy\" target=\"_blank\">React, and Redux</a>",
-      "."
-      ]
-    },
-  career: {
-    content: [
-      "Fun fact about my ",
-      "<a title=\"career\" href=\"https://www.linkedin.com/in/rachel-salois-a07a2320/\" target=\"_blank\">career</a>",
-      ":",
-      " I have read (for work) more post-apocalyptic novels than anyone I know",
-      "."
-      ]
-    },
-  cactus: {
-    content: [
-      "Occasionally",
-      ",",
-      " in my spare time",
-      ",",
-      " I care for a cactus that lives at my apartment",
-      "."
-    ]
-  },
-  look: {
-    content: [
-      "This is pretty much what it looks like",
-      ",",
-      " give or take a few prickles",
-      "."
-   ]
-  }
-}
+const HUMAN_CONTENT = `<p>I like to make cool websites.</p>
+
+<p>On my <a title='blog' href="https://rachelscodenotes.wordpress.com/" target='_blank'>blog</a>, I write about some of my more interesting programming-related discoveries.</p>
+
+<p>Sometimes <a title='twitter' href='https://twitter.com/rachel_salois' target='_blank'>tweet</a>.</p>
+
+<p>I've made some fun projects using <a title='rails' href='https://github.com/bobertoo/pretty-kitty-city' target='_blank'>Rails</a>, <a title="javascript" href="https://github.com/rachelsa/birthday-plaid" target="_blank">JavaScript</a>, <a title="React-Redux" href="https://github.com/acohn93/react-redux-playlist-buddy" target="_blank">React, and Redux</a>.</p>
+
+<p>Fun fact about my <a title="career" href="https://www.linkedin.com/in/rachel-salois-a07a2320/" target="_blank">career</a>: I have read (for work) more post-apocalyptic novels than anyone I know.</p>
+
+<p>Occasionally, in my spare time, I care for a cactus that lives at my apartment.</p>
+
+<p>This is pretty much what it looks like, give or take a few prickles.</p>`
+
+
+const CAT_CONTENT = `<p>Meow meow meow meow meow meow.</p>
+
+<p>Meow meow <a title='blog' href=\"https://rachelscodenotes.wordpress.com/" target='_blank'>blog</a>, meow meow meow meow meow meow meow meow meow-meow meow.</p>
+
+<p>Meow <a title='twitter' href='https://twitter.com/rachel_salois' target='_blank'>tweet</a>.</p>
+
+<p>Meow meow meow meow meow meow <a title='rails' href='https://github.com/bobertoo/pretty-kitty-city' target='_blank'>Rails</a>, <a title="javascript" href="https://github.com/rachelsa/birthday-plaid" target="_blank">JavaScript</a>, <a title="React-Redux" href="https://github.com/acohn93/react-redux-playlist-buddy" target="_blank">React, and Redux</a>.</p>
+
+<p>Meow meow meow meow <a title="career" href="https://www.linkedin.com/in/rachel-salois-a07a2320/" target="_blank">career</a>: meow meow meow (meow meow) meow meow-meow meow meow meow meow meow.</p>
+
+<p>Meow, meow meow meow meow, meow meow meow meow meow meow meow meow meow meow.</p>
+
+<p>Meow meow meow meow meow meow meow meow, meow meow meow meow meow meow.</p>`
--- index.js
+++ index.js
@@ -1,123 +1,33 @@
+const textColumn = document.getElementById("inner-text")
+const catModeEl = document.getElementById("cat-mode")
 
-$.getScript( "content.js", function() {
+window.onload = () => {
   console.log( "What is the difference between a cat and a comma?" )
-  $( document ).ready(function(){
-  console.log( "One has the paws before the claws and the other has the clause before the pause." )
-  $textColumn.append(loadContent())
-  }
-)
-})
-
-
-
-$catMode = $( "#cat-mode" )
-$textColumn = $( "#inner-text" )
-
-$catMode.click(checkMode)
+  window.addEventListener("click", checkMode)
+  humanMode()
+}
 
 function checkMode(){
-  if ($catMode.attr('id') === "cat-mode"){
+  if (catModeEl.id === "cat-mode"){
     catMode()
-  } 
+  }
   else {
     humanMode()
-  } 
-}
 
-function loadContent(){
-  var fullContent = ""
-  $.each(CONTENT, function(index, value) {
-    fullContent += "<p>"
-    var val = value["content"]
-    var partialContent = val.join("")
-    fullContent += partialContent
-    fullContent += "</p>"
-   }) 
-  return fullContent
+  }
 }
-
 
 function catMode(){
-  $catMode.html("<a href=\"#\">human mode</a>")
-  $catMode.prop('id', 'human-mode')
-  $textColumn.empty()
-  $textColumn.append( catContent() )  
+  console.log( "One has the paws before the claws and the other has the clause before the pause." )
+  catModeEl.innerHTML = "<a href='#'>human mode</a>"
+  catModeEl.id = 'human-mode'
+  textColumn.innerHTML = ""
+  textColumn.innerHTML = CAT_CONTENT
 }
 
 function humanMode(){
-  $catMode.html("<a href=\"#\">cat mode</a>")
-  $catMode.prop('id', 'cat-mode')
-  $textColumn.empty()
-  $textColumn.append( loadContent() )
+  catModeEl.innerHTML = "<a href='#'>cat mode</a>"
+  catModeEl.id = 'cat-mode'
+  textColumn.innerHTML = ""
+  textColumn.innerHTML = HUMAN_CONTENT
 }
-
-function catContent(){
-  var fullContent = ""
-  $.each(CONTENT, function(index, value) {
-    fullContent += "<p>"
-    var val = value["content"]
-    var partialContent = ""
-    for (var part in val){
-      if (isLink(val[part])) { 
-        partialContent += ` ${val[part]}`}
-      else if (isWords(val[part])) {
-        partialContent += ` ${meowify(val[part])}`}
-      else if (isPunct(part)) {
-        partialContent += val[part]}
-      else {
-        partialContent += val[part]}  
-    }
-    fullContent += capitalize(partialContent)
-    fullContent += "</p>"
-   }) 
-  return fullContent
-}
-
-function isLink(string){
-  return string.includes("<a")
-}
-
-function isWords(string){
-  var letters = /[a-zA-Z]/g
-  return letters.test(string)
-}
-
-function isPunct(string){
-  var punct = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g
-  return punct.test(string)
-}
-
-function meowify(text){
-  var len = text.split(" ").length
-  var meows = Array.apply(null, Array(len)).map(function () {return "meow"})
-  return meows.join(" ")
-}
-
-function capitalize(text){
-  var sentences = text.split(". ")
-  var capitalized = sentences.map(function(sentence){
-    var cap = sentence[1].toUpperCase()
-    return cap + sentence.substr(2, sentence.length - 1)
-  })
-return capitalized[0]
-}
-
-
-// function findPunct(text){
-//   var regex = /[.,\/#!$%\^&\*;:{}=\-_`~()]/g, result, indices = []
-//   while ( (result = regex.exec(text)) ) {
-//     indices.push(result.index);
-//   }
-//   return indices
-// }
-
-// function linkSubstrings(id){
-//   var text = document.getElementById(id).innerHTML
-//   var indices = findLinks(id), end = text.length, links = []
-
-//   for (index in indices){
-//     endIndex = text.substr(index, end).search(">")
-//     links.push([index, endIndex])
-//   } 
-//   return links
-// }
