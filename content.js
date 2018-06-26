7a123b5dbfe34a2316ce57117868a8cc48fb03e3
github username <rachel.salois@gmail.com>
2018-06-25 22:38:07 -0400

    WIP

--- content.js
+++ content.js
@@ -1,12 +1,18 @@
-const HUMAN_CONTENT = `<p>I like to make cool web apps.</p>
+const HUMAN_CONTENT = `<p>I like making web apps and writing about software development.</p>
 
-<p>On my <a title='blog' href="https://rachelscodenotes.wordpress.com/" target='_blank'>blog</a>, I write about some of my more interesting programming-related discoveries.</p>
+<p>Recently, I made <a href="https://github.com/RachelSa/rainward" target="_blank">Rainward</a>, the React app that helps you plan trips to dreary destinations. It has a <a href="https://floating-escarpment-37906.herokuapp.com/suggestions/" target="_blank">Rails API</a> with some snazzy <a href="https://github.com/RachelSa/rainward-api" target="_blank">documentation</a>.</a></p>
 
-<p>Sometimes <a title='twitter' href='https://twitter.com/rachel_salois' target='_blank'>tweet</a>.</p>
+<p>My <a title="javascript" href="http://rachelsalois.com/birthday-plaid/" target="_blank">Birthday Plaid</a> app uses plain old JS, CSS grid,
+ and a state-of-the-art PLAIDgorithm to generate a unique plaid for your birthday!
 
-<p>I've made some fun projects using <a title='rails' href='https://github.com/RachelSa/skcdc' target='_blank'>Rails</a>, <a title="javascript" href="http://rachelsalois.com/birthday-plaid/" target="_blank">JavaScript</a>, <a title="React-Redux" href="http://rachelsalois.com/rainward/" target="_blank">React</a>, and <a href="https://github.com/acohn93/react-redux-playlist-buddy" target="_blank">Redux</a>.</p>
+<p>Two things I'm thinking about lots lately:</p>
+<ol>
+  <li>state management (MobX, Redux)</li>
+  <li>test-driven development (Jest, Enzyme)</li>
+</ol>
 
-<p>Fun fact about my <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">career</a>: I have read (for work) more post-apocalyptic novels than anyone I know.</p>
+<p>Fun fact about my <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">career</a>:
+ I have read (for work) more post-apocalyptic novels than anyone I know.</p>
 
 <p>Occasionally, in my spare time, I care for a cactus that lives at my apartment.</p>
 
@@ -15,13 +21,15 @@ const HUMAN_CONTENT = `<p>I like to make cool web apps.</p>
 
 const CAT_CONTENT = `<p>Meow meow meow meow meow meow.</p>
 
-<p>Meow meow <a title='blog' href=\"https://rachelscodenotes.wordpress.com/" target='_blank'>blog</a>, meow meow meow meow meow meow meow meow meow-meow meow.</p>
+<p>Meow meow <a title='blog' href=\"https://rachelscodenotes.wordpress.com/" target='_blank'>blog</a>,
+ meow meow meow meow meow meow meow meow meow-meow meow.</p>
 
 <p>Meow <a title='twitter' href='https://twitter.com/rachel_salois' target='_blank'>tweet</a>.</p>
 
 <p>Meow meow meow meow meow meow <a title='rails' href='https://github.com/bobertoo/pretty-kitty-city' target='_blank'>Rails</a>, <a title="javascript" href="https://github.com/rachelsa/birthday-plaid" target="_blank">JavaScript</a>, <a title="React-Redux" href="https://github.com/acohn93/react-redux-playlist-buddy" target="_blank">React, and Redux</a>.</p>
 
-<p>Meow meow meow meow <a title="career" href="https://www.linkedin.com/in/rachel-salois-a07a2320/" target="_blank">career</a>: meow meow meow (meow meow) meow meow-meow meow meow meow meow meow.</p>
+<p>Meow meow meow meow <a title="career" href="https://www.linkedin.com/in/rachel-salois-a07a2320/" target="_blank">career</a>:
+ meow meow meow (meow meow) meow meow-meow meow meow meow meow meow.</p>
 
 <p>Meow, meow meow meow meow, meow meow meow meow meow meow meow meow meow meow.</p>
 
