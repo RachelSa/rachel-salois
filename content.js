c9bb90795dec04e8a2be270482847bfbe5cac6fa
github username <rachel.salois@gmail.com>
2018-07-02 20:48:42 -0400

    update cat mode and human

--- content.js
+++ content.js
@@ -1,15 +1,6 @@
 const HUMAN_CONTENT = `<p>I like making web apps and writing about software development.</p>
 
-<p>Recently, I made <a href="https://github.com/RachelSa/rainward" target="_blank">Rainward</a>, the React app that helps you plan trips to dreary destinations. It has a <a href="https://floating-escarpment-37906.herokuapp.com/suggestions/" target="_blank">Rails API</a> with some snazzy <a href="https://github.com/RachelSa/rainward-api" target="_blank">documentation</a>.</a></p>
-
-<p>My <a title="javascript" href="http://rachelsalois.com/birthday-plaid/" target="_blank">Birthday Plaid</a> app uses plain old JS, CSS grid,
- and a state-of-the-art PLAIDgorithm to generate a unique plaid for your birthday!
-
-<p>Two things I'm thinking about lots lately:</p>
-<ol>
-  <li>state management (MobX, Redux)</li>
-  <li>test-driven development (Jest, Enzyme)</li>
-</ol>
+<p>Recently, I've been working on <a href="https://github.com/RachelSa/rainward" target="_blank">this React app</a> that helps you plan trips to dreary and drizzly destinations. It has a <a href="https://floating-escarpment-37906.herokuapp.com/suggestions/" target="_blank">Rails API</a> with some <a href="https://github.com/RachelSa/rainward-api" target="_blank">snazzy documentation</a>.</a></p>
 
 <p>Fun fact about my <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">career</a>:
  I have read (for work) more post-apocalyptic novels than anyone I know.</p>
@@ -19,16 +10,11 @@ const HUMAN_CONTENT = `<p>I like making web apps and writing about software deve
 <p>This is pretty much what it looks like, give or take a few prickles.</p>`
 
 
-const CAT_CONTENT = `<p>Meow meow meow meow meow meow.</p>
+const CAT_CONTENT = `<p>Meow meow meow meow meow meow meow meow meow meow.</p>
 
-<p>Meow meow <a title='blog' href=\"https://rachelscodenotes.wordpress.com/" target='_blank'>blog</a>,
- meow meow meow meow meow meow meow meow meow-meow meow.</p>
+<p>Meow, meow meow meow meow <a href="https://github.com/RachelSa/rainward" target="_blank">this React app</a> meow meow meow meow meow meow meow meow meow meow. Meow meow meow <a href="https://floating-escarpment-37906.herokuapp.com/suggestions/" target="_blank">Rails API</a> meow meow <a href="https://github.com/RachelSa/rainward-api" target="_blank">snazzy documentation</a>.</a></p>
 
-<p>Meow <a title='twitter' href='https://twitter.com/rachel_salois' target='_blank'>tweet</a>.</p>
-
-<p>Meow meow meow meow meow meow <a title='rails' href='https://github.com/bobertoo/pretty-kitty-city' target='_blank'>Rails</a>, <a title="javascript" href="https://github.com/rachelsa/birthday-plaid" target="_blank">JavaScript</a>, <a title="React-Redux" href="https://github.com/acohn93/react-redux-playlist-buddy" target="_blank">React, and Redux</a>.</p>
-
-<p>Meow meow meow meow <a title="career" href="https://www.linkedin.com/in/rachel-salois-a07a2320/" target="_blank">career</a>:
+<p>Meow meow meow meow <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">career</a>:
  meow meow meow (meow meow) meow meow-meow meow meow meow meow meow.</p>
 
 <p>Meow, meow meow meow meow, meow meow meow meow meow meow meow meow meow meow.</p>
