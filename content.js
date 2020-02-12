9b6c05a70e6d087427c067322ef165ec55bc77e8
RacheSa <rachel.salois@gmail.com>
2020-02-12 12:33:30 -0500

    Feb 2020 updates

--- content.js
+++ content.js
@@ -1,19 +1,19 @@
-const HUMAN_CONTENT = `<p>I like making web apps and writing about software development.</p>
+const HUMAN_CONTENT = `<p>I'm a software engineer and technical writer.</p>
 
-<p>Recently, I've been working on <a href="https://github.com/RachelSa/rainward" target="_blank">this React app</a> that helps you plan trips to dreary and drizzly destinations. It has a <a href="https://floating-escarpment-37906.herokuapp.com/suggestions/" target="_blank">Rails API</a> with some <a href="https://github.com/RachelSa/rainward-api" target="_blank">snazzy documentation</a>.</a></p>
+<p>I am the creator of <a href="http://rachelsalois.com/birthday-plaid/" target="_blank">Birthday Plaid</a>, a JavaScript app that generates exactly the right plaid for YOUR birthday, built with a state of-the-art PLAIDgorithm.</p>
 
-<p>Fun fact <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">about my career</a>: aside from working as a software engineer and technical writer, I also review and write about children's books.</p>
+<p>Fun fact <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">about my career</a>: I used to review and write about children's books.</p>
 
 <p>Occasionally, in my spare time, I care for a cactus that lives at my apartment.</p>
 
 <p>This is pretty much what it looks like, give or take a few prickles.</p>`
 
 
-const CAT_CONTENT = `<p>Meow meow meow meow meow meow meow meow meow meow.</p>
+const CAT_CONTENT = `<p>Meow meow meow meow meow meow meow.</p>
 
-<p>Meow, meow meow meow meow <a href="https://github.com/RachelSa/rainward" target="_blank">this React app</a> meow meow meow meow meow meow meow meow meow meow. Meow meow meow <a href="https://floating-escarpment-37906.herokuapp.com/suggestions/" target="_blank">Rails API</a> meow meow <a href="https://github.com/RachelSa/rainward-api" target="_blank">snazzy documentation</a>.</a></p>
+<p>Meow meow meow meow meow <a href="http://rachelsalois.com/birthday-plaid/" target="_blank">Birthday Plaid</a>, meow meow meow meow meow meow meow meow meow meow MEOW meow, meow meow meow meow-meow-meow-meow MEOW-meow.</p>
 
-<p>Meow meow <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">about my career</a>: meow meow meow meow meow meow meow meow meow meow, meow meow meow meow meow meow meow meow.</p>
+<p>Meow meow <a title="career" href="https://www.linkedin.com/in/rachel-salois/" target="_blank">about my career</a>: meow meow meow meow meow meow meow meow meow.</p>
 
 <p>Meow, meow meow meow meow, meow meow meow meow meow meow meow meow meow meow.</p>
 
